33557440a93d77abc2e2527418e3037628642c2e
xiaoysec <xiaoysec@163.com>
2017-07-04 16:47:36 +0800

    添加用户登陆拦截器

--- src/main/java/com/xiaoysec/hrm/sys/interceptor/LoginInterceptor.java
+++ src/main/java/com/xiaoysec/hrm/sys/interceptor/LoginInterceptor.java
@@ -3,8 +3,12 @@ package com.xiaoysec.hrm.sys.interceptor;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.servlet.HandlerInterceptor;
 import org.springframework.web.servlet.ModelAndView;
+
+import com.xiaoysec.hrm.business.user.entity.User;
 
 /**
  * 登陆拦截器
@@ -13,22 +17,41 @@ import org.springframework.web.servlet.ModelAndView;
  *
  */
 public class LoginInterceptor implements HandlerInterceptor {
-
-	public void afterCompletion(HttpServletRequest arg0, HttpServletResponse arg1, Object arg2, Exception arg3)
+	
+	private static Logger logger =  LoggerFactory.getLogger(LoginInterceptor.class); 
+	/**
+	 * 视图返回后调用
+	 */
+	public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object arg2, Exception arg3)
 			throws Exception {
 		// TODO Auto-generated method stub
 
 	}
 
-	public void postHandle(HttpServletRequest arg0, HttpServletResponse arg1, Object arg2, ModelAndView arg3)
+	/**
+	 * 从控制器返回视图中拦截，修改内容
+	 */
+	public void postHandle(HttpServletRequest request, HttpServletResponse response, Object obj, ModelAndView mv)
 			throws Exception {
 		// TODO Auto-generated method stub
 
 	}
 
-	public boolean preHandle(HttpServletRequest arg0, HttpServletResponse arg1, Object arg2) throws Exception {
-		// TODO Auto-generated method stub
-		return false;
+	/**
+	 * 返回false 请求将终止不会执行postHandle和afterCompletion
+	 * 返回true 请求将继续进行
+	 * Obj表示请求的目标控制器
+	 * 
+	 */
+	public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object obj) throws Exception {
+		logger.info("LoginInterceptor preHandle方法被调用...");
+		User user = (User) request.getSession().getAttribute("sessionUser");
+		if(user == null){
+			String path = request.getContextPath();
+			response.sendRedirect(path+"/a/login");
+			return false;
+		}
+		return true;
 	}
 
 }
